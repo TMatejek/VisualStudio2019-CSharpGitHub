2692abec99d7d2f482425b5500fe11773e89a9da
TMatejek <tmatejek@usf.edu>
2021-08-25 14:33:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2019_CSharpGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
